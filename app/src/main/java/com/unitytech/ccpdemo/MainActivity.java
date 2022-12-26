85a36137e95a7827681fae1aa6b423f6e2243007
yasirraheel <islammujahid921@gmail.com>
2022-12-26 23:13:07 +0300

    new properties added

--- app/src/main/java/com/unitytech/ccpdemo/MainActivity.java
+++ app/src/main/java/com/unitytech/ccpdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.unitytech.ccpdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hbb20.CCPCountry;
@@ -10,21 +11,26 @@ import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
     CountryCodePicker countryCodePicker;
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        countryCodePicker=findViewById(R.id.countryCode_picker);
+        countryCodePicker=findViewById(R.id.countryCodePicker);
+        textView=findViewById(R.id.codetv);
         countryCodePicker.setAutoDetectedCountry(true);
 
+        String DefaultCountry= countryCodePicker.getDefaultCountryName();
+        String DefaultcountCode = countryCodePicker.getDefaultCountryCode();
+
+
+        textView.setText("Default Country is "+DefaultCountry+" and Code is "+DefaultcountCode);
         countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
             public void onCountrySelected() {
-                String cc = countryCodePicker.getSelectedCountryCode();
-                String countryname = countryCodePicker.getSelectedCountryName();
-                Toast.makeText(MainActivity.this, "Country selected "+cc+"" +
-                        "and Name is "+countryname, Toast.LENGTH_SHORT).show();
+                String CountryName = countryCodePicker.getSelectedCountryName();
+                String CountryCode = countryCodePicker.getSelectedCountryCode();
+                textView.setText("Selected Country is "+CountryName+" and Code is "+CountryCode);
             }
         });
 
